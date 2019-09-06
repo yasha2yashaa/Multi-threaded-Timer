79849388d7d025f28f614fcf979b7c690c7aaeb5
yasha2yashaa <yasha2yashaa@gmail.com>
2019-09-06 23:54:35 +0200

    Added constructor and getUserInput() method

--- src/main/java/helpers/InputTaker.java
+++ src/main/java/helpers/InputTaker.java
@@ -1,4 +1,17 @@
 package helpers;
 
+import models.UserInput;
+
+import java.util.Scanner;
+
 public class InputTaker {
+    private Scanner scanner;
+
+    public InputTaker() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public UserInput getUserInput() {
+        return new UserInput(scanner.nextLine());
+    }
 }
