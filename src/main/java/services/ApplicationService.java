be489ce2d89c48abf6d67c584b4e727e1499b84b
yasha2yashaa <yasha2yashaa@gmail.com>
2019-09-07 01:36:06 +0200

    Added public methods mocks to ApplicationService

--- src/main/java/services/ApplicationService.java
+++ src/main/java/services/ApplicationService.java
@@ -1,4 +1,22 @@
 package services;
 
+import view.ThreadInfoPrinter;
+
 public class ApplicationService {
+    private ThreadInfoPrinter threadInfoPrinter;
+
+    public ApplicationService(ThreadInfoPrinter threadInfoPrinter) {
+        this.threadInfoPrinter = threadInfoPrinter;
+    }
+
+    public void show() {
+    }
+
+    public void startNewTimer(String timerName) {
+
+    }
+
+    public void pauseTimer(String timerName) {
+
+    }
 }
