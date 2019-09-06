ca6980dce2146d55d07a2a891717222a86a3d867
yasha2yashaa <yasha2yashaa@gmail.com>
2019-09-07 01:57:50 +0200

    Finished Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import controllers.ApplicationController;
+import helpers.InputTaker;
+import helpers.TimersContainer;
+import services.ApplicationService;
+import view.ThreadInfoPrinter;
 
 public class Main {
+    public static void main(String[] args) {
+        TimersContainer timersContainer = new TimersContainer();
+        ThreadInfoPrinter threadInfoPrinter = new ThreadInfoPrinter();
+        ApplicationService applicationService = new ApplicationService(timersContainer, threadInfoPrinter);
+        InputTaker inputTaker = new InputTaker();
+        ApplicationController applicationController = new ApplicationController(applicationService, inputTaker);
+
+        applicationController.start();
+    }
 }
