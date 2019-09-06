97fe053d58d452107263747ddbd5c7816048c276
yasha2yashaa <yasha2yashaa@gmail.com>
2019-09-07 00:03:09 +0200

    Finished UserInput class

--- src/main/java/models/UserInput.java
+++ src/main/java/models/UserInput.java
@@ -7,4 +7,33 @@ public class UserInput {
     public UserInput(String input) {
         this.input = input;
     }
+
+    public boolean isExitRequest() {
+        return input.equalsIgnoreCase("exit");
+    }
+
+    public boolean isShowRequest() {
+        return input.equalsIgnoreCase("show");
+    }
+
+    public boolean isStartRequest() {
+        return input.toLowerCase().startsWith("start");
+    }
+
+    public boolean isStopRequest() {
+        return input.toLowerCase().startsWith("stop");
+    }
+
+    public boolean isPauseRequest() {
+        return input.toLowerCase().startsWith("pause");
+    }
+
+    public String getTimerName() {
+        if (isStartRequest() || isStopRequest() || isPauseRequest()) {
+            return input.split(" ")[1];
+        }
+        else {
+            throw new IllegalStateException("Input is not a start, stop or pause request, it doesn't contain timer name.");
+        }
+    }
 }
