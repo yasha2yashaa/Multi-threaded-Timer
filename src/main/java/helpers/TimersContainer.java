bcce481e7cdc1d3a88435cfac20bd76931d94e93
yasha2yashaa <yasha2yashaa@gmail.com>
2019-09-07 01:34:59 +0200

    Finished TimersContainer class

--- src/main/java/helpers/TimersContainer.java
+++ src/main/java/helpers/TimersContainer.java
@@ -1,4 +1,23 @@
 package helpers;
 
+import java.util.HashMap;
+
 public class TimersContainer {
+    private HashMap<String, Timer> timers;
+
+    public TimersContainer() {
+        timers = new HashMap<>();
+    }
+
+    public void add(String name, Timer timer) {
+        timers.put(name, timer);
+    }
+
+    public Timer get(String name) {
+        return timers.get(name);
+    }
+
+    public boolean exists(String name) {
+        return timers.containsKey(name);
+    }
 }
