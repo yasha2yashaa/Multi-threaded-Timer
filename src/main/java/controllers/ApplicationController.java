1d5e73ff73dabbe69151baf8aa9f29f62da1d58b
yasha2yashaa <yasha2yashaa@gmail.com>
2019-09-07 00:59:19 +0200

    Finished ApplicationController loop and added service method initializers mocks

--- src/main/java/controllers/ApplicationController.java
+++ src/main/java/controllers/ApplicationController.java
@@ -1,4 +1,52 @@
 package controllers;
 
+import helpers.InputTaker;
+import models.UserInput;
+import services.ApplicationService;
+
 public class ApplicationController {
+    private ApplicationService applicationService;
+    private InputTaker inputTaker;
+    private boolean run;
+
+    public ApplicationController(ApplicationService applicationService, InputTaker inputTaker) {
+        this.applicationService = applicationService;
+        this.inputTaker = inputTaker;
+    }
+
+    public void start() {
+        run = true;
+        UserInput input;
+        while (run) {
+            input = inputTaker.getUserInput();
+            if (input.isExitRequest()) {
+                exit();
+            }
+            else if (input.isShowRequest()) {
+                show();
+            }
+            else if (input.isStartRequest()) {
+                start(input.getTimerName());
+            }
+            else if (input.isStopRequest()) {
+                stop(input.getTimerName());
+            }
+        }
+    }
+
+    private void exit() {
+        run = false;
+    }
+
+    private void show() {
+
+    }
+
+    private void start(String timerName) {
+
+    }
+
+    private void stop(String timerName) {
+
+    }
 }
