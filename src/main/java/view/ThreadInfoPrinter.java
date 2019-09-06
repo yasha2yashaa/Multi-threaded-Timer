0165f99ef6807d2c8e84a92ec79977a7800e4cc3
yasha2yashaa <yasha2yashaa@gmail.com>
2019-09-07 01:45:40 +0200

    Finished ThreadInfoPrinter class

--- src/main/java/view/ThreadInfoPrinter.java
+++ src/main/java/view/ThreadInfoPrinter.java
@@ -1,4 +1,35 @@
 package view;
 
+import helpers.Timer;
+import helpers.TimersContainer;
+
+import java.util.Set;
+
 public class ThreadInfoPrinter {
+    public void print(TimersContainer timersContainer) {
+        Set<String> timerNames = timersContainer.getTimerNames();
+        Timer timer;
+        for (String timerName: timerNames) {
+            timer = timersContainer.get(timerName);
+            print("Name: ");
+            print(timerName);
+            print(", ThreadId: ");
+            print(timer.getThreadId());
+            print(", Seconds: ");
+            print(timer.getSecondCounter());
+            print("\n");
+        }
+    }
+
+    private void print(String string) {
+        System.out.print(string);
+    }
+
+    private void print(long number) {
+        System.out.print(number);
+    }
+
+    private void print(int number) {
+        System.out.println(number);
+    }
 }
